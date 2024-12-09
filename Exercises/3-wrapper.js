55369102f3611e624539e225e112ca3140c722e6
Diana <navbidibi@gmail.com>
2024-12-09 21:36:36 +0200

    The tests are done

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  for (let i = 0; i < types - 1; i++) {
+    if (typeof args[i] !== typeof types[i](args[i])) throw new Error("TypeError ");      
+  }
+  const result = fn(...args);
+  if (typeof result !== typeof types[types.length - 1](result)) throw new Error("TypeError "); 
+  return result;
+};
 
 module.exports = { contract };
